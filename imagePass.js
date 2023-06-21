07cc5b28cbe0738f212180da0dd6ceaa7541ed2a
Terandox <rene-gilbert99@web.de>
2023-06-21 10:44:53 +0200

    Limited input to only images

--- imagePass.js
+++ imagePass.js
@@ -1,4 +1,4 @@
-const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.+-_";
+const iPCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.+-_";
 
 const CheckPassfield = () => {
     let passwordField = document.querySelectorAll("input[type=password]") || null;
@@ -20,6 +20,7 @@ const ReplacePasswordField = (field) => {
     let name = field.id === null ? field.name : null;
     const NewPasswordField = document.createElement("input");
     NewPasswordField.type = "file";
+    NewPasswordField.accept = "image/png, image/gif, image/jpeg";
     NewPasswordField.style = styles;
     NewPasswordField.classList = classes;
     NewPasswordField.id = (id !== null ? id : name) + "-image-field";
@@ -46,8 +47,8 @@ const GenerateNewPassword = (seed) => {
     let newRng = new Math.seedrandom(seed);
     let newPassword = "";
     let length = 32;
-    for (var i = 0, n = characters.length; i < length; ++i) {
-        newPassword += characters.charAt(Math.floor(newRng() * n));
+    for (var i = 0, n = iPCharacters.length; i < length; ++i) {
+        newPassword += iPCharacters.charAt(Math.floor(newRng() * n));
     }
     return newPassword;
 };
